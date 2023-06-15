f43e65fb47b16013b0e851c2e7e5bc8456f1496f
Andrew Cranford <acranford25@gmail.com>
2023-06-15 12:27:06 -0400

    createTables

--- db/seed.js
+++ db/seed.js
@@ -11,6 +11,60 @@ async function dropTables() {
 async function createTables() {
   console.log("Creating tables...");
   try {
+    await client.query(`CREATE TABLE "Users" (
+      "id" int   NOT NULL,
+      "isAdmin" boolean   NOT NULL,
+      "loggedIn" boolean   NOT NULL,
+      "username" text   NOT NULL,
+      "password" text   NOT NULL,
+      CONSTRAINT "pk_Users" PRIMARY KEY (
+          "id"
+       )
+  );`);
+    await client.query(`
+    CREATE TABLE "Items" (
+      "id" int   NOT NULL,
+      "name" text   NOT NULL,
+      "description" text   NOT NULL,
+      "cost" int   NOT NULL,
+      "categoryId" int   NOT NULL,
+      "available" boolean   NOT NULL,
+      "tags" text   NOT NULL,
+      CONSTRAINT "pk_Items" PRIMARY KEY (
+          "id"
+       )
+  );`);
+    await client.query(`
+    CREATE TABLE "Category" (
+      "id" int   NOT NULL,
+      "name" text   NOT NULL,
+      "description" text   NOT NULL,
+      CONSTRAINT "pk_Category" PRIMARY KEY (
+          "id"
+       )
+  );`);
+    await client.query(`
+    CREATE TABLE "Order_Items" (
+      "id" int   NOT NULL,
+      "itemId" int   NOT NULL,
+      "item_quantity" int   NOT NULL,
+      "price" int   NOT NULL,
+      "orderId" int   NOT NULL,
+      CONSTRAINT "pk_Order_Items" PRIMARY KEY (
+          "id"
+       )
+  );
+    `);
+    await client.query(`
+    CREATE TABLE "Order" (
+      "id" int   NOT NULL,
+      "usersId" int   NOT NULL,
+      "totalPrice" int   NOT NULL,
+      CONSTRAINT "pk_Order" PRIMARY KEY (
+          "id"
+       )
+  );
+    `);
   } catch (error) {
     console.log(error);
   }
