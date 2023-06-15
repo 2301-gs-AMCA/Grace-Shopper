fce87b1636da5cf99070c36fb7b303ff303e7c68
Andrew <andrewboug@hotmail.com>
2023-06-15 15:52:46 -0500

    added AuthProvider component and made some async functions in auth.js

---
+++ client/src/components/AuthProvider.jsx
@@ -0,0 +1,33 @@
+import { createContext, useState, useEffect } from "react";
+import { fetchMe } from "../api/auth";
+// Create the context
+export const AuthContext = createContext();
+
+// Create our Provider (wrapper component)
+const AuthProvider = ({ children }) => {
+  const [token, setToken] = useState(localStorage.getItem("token"));
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    async function getMe() {
+      const APIResponse = await fetchMe(token);
+      setUser(APIResponse.data);
+    }
+    if (token) {
+      getMe();
+    }
+  }, [token]);
+
+  const contextValue = {
+    token,
+    setToken,
+    user,
+    setUser,
+  };
+
+  return (
+    <AuthContext.Provider value={contextValue}>{children}</AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
