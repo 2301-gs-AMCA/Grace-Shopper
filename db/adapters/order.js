8d5964b14af9cffa0bed38e1a8d5ff9936730ede
Andrew Cranford <acranford25@gmail.com>
2023-06-16 12:17:29 -0400

    order adapters

--- db/adapters/order.js
+++ db/adapters/order.js
@@ -1,1 +1,100 @@
+const client = require("../client");
 
+async function createOrder(userId, totalPrice) {
+  try {
+    const {
+      rows: [order],
+    } = await client.query(
+      `
+                INSERT INTO orders ("userId", totalPrice)
+                VALUES ($1, $2)
+                RETURNING *
+            `,
+      [userId, totalPrice]
+    );
+    return order;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function getOrderById(orderId) {
+  try {
+    const {
+      rows: [order],
+    } = await client.query(`
+            SELECT * from orders WHERE id=${orderId}
+            ORDER by id;
+        `);
+    if (!order) {
+      return null;
+    }
+    return order;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function getAllUsersOrders(userId) {
+  try {
+    const { rows: orders } = await client.query(
+      `
+                SELECT ords.id, ords.userId, ords.totalPrice,
+                CASE WHEN orditms.orderId IS NULL THEN '[]'::json
+                ELSE
+                JSON_AGG(
+                    JSON_BUILD_OBJECT (
+                        'id', itms.id,
+                        'name', itms.name,
+                        'description', itms.description,
+                        'cost', itms.cost,
+                        'categoryId', itms.categoryId,
+                        'isAvailable', itms.isAvailable,
+                        'tags', itms.tags,
+                        'item_quantity', orditms.item_quantity,
+                        'price', orditms.price
+                    )
+                ) END AS items
+                FROM orders ords
+                FULL OUTER JOIN order_items orditms
+                    ON ords.id = orditms.itemId
+                FULL OUTER JOIN items itms
+                    ON orditms.itemId = itms.id
+                JOIN users us
+                    ON us.id = ords.userId
+                WHERE us.id = $1
+                GROUP BY ords.id, orditms.orderId
+            `,
+      [userId]
+    );
+    return orders;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function updateOrder(orderId, totalPrice) {
+  try {
+    const {
+      rows: [order],
+    } = await client.query(
+      `
+                UPDATE orders
+                SET totalprice = $2
+                WHERE id = $1
+                RETURNING *
+            `,
+      [orderId, totalPrice]
+    );
+    return order;
+  } catch (error) {
+    throw error;
+  }
+}
+
+module.exports = {
+  createOrder,
+  getAllUsersOrders,
+  getOrderById,
+  updateOrder,
+};
