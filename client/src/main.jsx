649f719e4d284e1185c6ec4791f4a5781d6a1ec6
Andrew Cranford <acranford25@gmail.com>
2023-06-22 12:22:29 -0400

    dubugged

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-import AuthProvider from "./components/Auth/AuthProvider.jsx";
+import AuthProvider from "./Components/AuthProvider.jsx";
 
 import "./index.css";
 
