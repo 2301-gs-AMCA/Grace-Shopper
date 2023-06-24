208ef8e1ab12cc57e9023a11b3c50a4aef58fdca
Matthew Rowley <mattrowley10@gmail.com>
2023-06-24 09:27:37 -0600

    profile jsx and css

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -1,14 +1,30 @@
 import useAuth from "../hooks/useAuth";
 
-
 export default function Profile() {
-  const {user} = useAuth();
-  console.log(user)
-  return <div className="profile">
-  <h1>profile</h1>
-  <div>
-    <h2>{user.username}</h2>
-    <img src="" alt="" />
-  </div>
-  </div>;
+  const { user } = useAuth();
+  console.log(user);
+  return (
+    <div className="profile">
+      <h1 className="userHeader">Welcome, {user.username}!</h1>
+      <div className="userInfo">
+        <ul className="userInfo">
+          <li>{user.username}</li>
+          <li>security info</li>
+          <li>settings</li>
+        </ul>
+      </div>
+      <div className="itemsULike">
+        <h2>Items You May Like</h2>
+        <ul className="likedItems">
+          <li>List of items based on your search history</li>
+        </ul>
+      </div>
+      <div className="orderHistory">
+        <ul className="history">
+          <h2>Order History</h2>
+          <li className="historyItems">Your Recently Ordered Items</li>
+        </ul>
+      </div>
+    </div>
+  );
 }
