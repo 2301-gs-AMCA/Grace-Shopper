2df4a2629d10c2583c988d42d99bfcb723c83a79
Matthew Rowley <mattrowley10@gmail.com>
2023-07-13 11:14:21 -0600

    order history -  in progress

--- client/src/ProfileLinks/OrderHistory.jsx
+++ client/src/ProfileLinks/OrderHistory.jsx
@@ -1,23 +1,46 @@
 import React from "react";
 import { getMyOrders } from "../api/orders";
+import { getOrderItems } from "../api/order_items";
 import { useState, useEffect } from "react";
+import useAuth from "../hooks/useAuth";
 
 export default function OrderHistory() {
+  const { user } = useAuth();
   const [myOrders, setMyOrders] = useState([]);
+  const [orderItems, setOrderItems] = useState();
+  const [orderId, setOrderId] = useState(0);
 
   async function getOrders() {
-    let order = await getMyOrders();
-    console.log("my orders", order);
-    setMyOrders(order);
+    let result = await getMyOrders();
+    console.log("my orders", result.orders);
+    setMyOrders(result.orders);
+    myOrders.map((order) => {
+      setOrderId(order.id);
+    });
   }
+
   useEffect(() => {
     getOrders();
   }, []);
-  // myOrders.map((orders) => {
-  //   return (
-  //     <div className="order-history" key="order.id">
-  //       order.name
-  //     </div>
-  //   );
-  // });
+
+  return (
+    <div className="order-history">
+      {myOrders.map((order) => {
+        return (
+          <div>
+            {order.id}
+            {order.items.map((item) => {
+              console.log(item.name);
+              return (
+                <ul className="order-history-list">
+                  <li>{item.name}</li>
+                  <li>${item.cost}</li>
+                </ul>
+              );
+            })}
+          </div>
+        );
+      })}
+    </div>
+  );
 }
