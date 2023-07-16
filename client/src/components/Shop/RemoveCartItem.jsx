da121115b4e28e6d7dc3c0beff6db63cb2966d9c
Andrew Cranford <acranford25@gmail.com>
2023-07-16 16:11:29 -0400

    modal message to remove item

--- client/src/components/Shop/RemoveCartItem.jsx
+++ client/src/components/Shop/RemoveCartItem.jsx
@@ -1,7 +1,94 @@
 import { deleteOrderItem } from "../../api/order_items";
 import useCart from "../../hooks/useCart";
+import { Modal, Button } from "bootstrap";
+import { Row, Col, Container, Card, Table, Alert } from "react-bootstrap";
+import { useState } from "react";
+import RemoveConfirmation from "./RemoveConfirmation";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-export default function RemoveCartItem({ item }) {
+const DeleteConfirmation = ({
+  showModal,
+  hideModal,
+  confirmModal,
+  item,
+  message,
+}) => {
+  return (
+    <Modal show={showModal} onHide={hideModal}>
+      <Modal.Header closeButton>
+        <Modal.Title>Remove Confirmation</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <div className="alert alert-danger">{message}</div>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="default" onClick={hideModal}>
+          Cancel
+        </Button>
+        <Button variant="danger" onClick={() => confirmModal(item)}>
+          Remove
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+};
+
+export default function RemoveCartItems({ item }) {
+  const { setCart, isCounted, setIsCounted } = useCart();
+  const [displayConfirmationModal, setDisplayConfirmationModal] =
+    useState(false);
+  const [deleteMessage, setDeleteMessage] = useState(null);
+
+  const showDeleteModal = (item) => {
+    setDeleteMessage(
+      `Are you sure you want to remove the item '${item.name}'?`
+    );
+
+    setDisplayConfirmationModal(true);
+  };
+
+  const hideConfirmationModal = () => {
+    setDisplayConfirmationModal(false);
+  };
+
+  const submitDelete = (item) => {
+    setDisplayConfirmationModal(false);
+    async function removeItem() {
+      const order = await deleteOrderItem(item.order_item_id);
+      setCart(order);
+      setIsCounted(!isCounted);
+    }
+    removeItem();
+  };
+
+  return (
+    <Container className="mt-4">
+      <Row>
+        <Col md={{ span: 10, offset: 1 }}>
+          <Card className="mt-2">
+            <Card.Body striped bordered hover size="sm">
+              <button
+                type="onClick"
+                onClick={() => showDeleteModal("item", item)}
+              >
+                Remove
+              </button>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+      <RemoveConfirmation
+        showModal={displayConfirmationModal}
+        confirmModal={submitDelete}
+        hideModal={hideConfirmationModal}
+        item={item}
+        message={deleteMessage}
+      />
+    </Container>
+  );
+}
+
+/*export default function RemoveCartItem({ item }) {
   const { setCart, isCounted, setIsCounted } = useCart();
   return (
     <form
@@ -20,4 +107,4 @@ export default function RemoveCartItem({ item }) {
       <button type="onClick">Remove</button>
     </form>
   );
-}
+}*/
